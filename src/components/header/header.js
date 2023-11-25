e675586de0dfa309347a403578bb18f00871ffd2
pwnmeplzthx <pshumakov11@mail.ru>
2023-11-25 13:51:10 +0300

    header layout

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from "../../common/DivComponent";
+import './header.css';
 
 export class Header extends DivComponent {
     constructor(appState) {
@@ -12,6 +13,19 @@ export class Header extends DivComponent {
         this.el.innerHTML = `
             <div>
                 <img src="/static/logo.svg" alt="Логотип" />
+            </div>
+            <div class="menu">
+                <a class="menu-item" href="#">
+                    <img src="/static/search.svg" alt="Поиск иконка" />
+                    Поиск скниг
+                </a>
+                <a class="menu-item" href="#favorites">
+                    <img src="/static/favorites.svg" alt="Избранное иконка" />
+                    Избранное
+                    <div class="menu-counter">
+                        ${this.appState.favorites.length}
+                    </div>
+                </a>
             </div>
         `
 
